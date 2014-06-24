bd10e08a79e52188ade10f1b62e204cb51f2e1c4
Ian Kane <iankane@example.com>
2014-06-23 17:22:01 -0700

    added HIV data, test on training set

--- FFN.py
+++ FFN.py
@@ -4,74 +4,82 @@ from pybrain.structure import FullConnection
 from pybrain.structure import BiasUnit
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
-import numpy as np
+from numpy import *
+import csv
 
-# def placeDataIntoArray(fileName):
-#     with open(fileName, mode='rbU') as csvfile:
-#         datareader = csv.reader(csvfile, delimiter=',', quotechar=' ')
-#         dataArray = array([row for row in datareader], dtype=float64, order='C')
+def placeDataIntoArray(fileName):
+    with open(fileName, mode='rbU') as csvfile:
+        datareader = csv.reader(csvfile, delimiter=',', quotechar=' ')
+        dataArray = array([row for row in datareader], dtype=float64, order='C')
 
-#     if (min(dataArray.shape) == 1): # flatten arrays of one row or column
-#         return dataArray.flatten(order='C')
-#     else:
-#         return dataArray
+    if (min(dataArray.shape) == 1): # flatten arrays of one row or column
+        return dataArray.flatten(order='C')
+    else:
+        return dataArray
 
-# def getAllOfTheData():
-#     TrainX = placeDataIntoArray('Train-Data.csv')
-#     TrainY = placeDataIntoArray('Train-pIC50.csv')
-#     ValidateX = placeDataIntoArray('Validation-Data.csv')
-#     ValidateY = placeDataIntoArray('Validation-pIC50.csv')
-#     TestX = placeDataIntoArray('Test-Data.csv')
-#     TestY = placeDataIntoArray('Test-pIC50.csv')
-#     return TrainX, TrainY, ValidateX, ValidateY, TestX, TestY
+def getAllOfTheData():
+    TrainX = placeDataIntoArray('Train-Data.csv')
+    TrainY = placeDataIntoArray('Train-pIC50.csv')
+    ValidateX = placeDataIntoArray('Validation-Data.csv')
+    ValidateY = placeDataIntoArray('Validation-pIC50.csv')
+    TestX = placeDataIntoArray('Test-Data.csv')
+    TestY = placeDataIntoArray('Test-pIC50.csv')
+    return TrainX, TrainY, ValidateX, ValidateY, TestX, TestY
 
-# def rescaleTheData(TrainX, TrainY, ValidateX, ValidateY, TestX, TestY):
+def rescaleTheData(TrainX, TrainY, ValidateX, ValidateY, TestX, TestY):
 
-#     # 1 degree of freedom means (ddof) N-1 unbiased estimation
-#     TrainXVar = TrainX.var(axis = 0, ddof=1)
-#     TrainXMean = TrainX.mean(axis = 0)
-
-#     for i in range(0, TrainX.shape[0]):
-#         TrainX[i,:] = (TrainX[i,:] - TrainXMean)/sqrt(TrainXVar)
+    # 1 degree of freedom means (ddof) N-1 unbiased estimation
+    TrainXVar = TrainX.var(axis = 0, ddof=1)
+    TrainXMean = TrainX.mean(axis = 0)
+    TrainYVar = TrainY.var(axis = 0, ddof=1)
+    TrainYMean = TrainY.mean(axis = 0)
+	
+    for i in range(0, TrainX.shape[0]):
+        TrainX[i,:] = (TrainX[i,:] - TrainXMean)/sqrt(TrainXVar)
     
-#     for i in range(0, TrainX.shape[0]):
-#         TrainY[i] = (TrainY[i] - TrainYMean)/sqrt(TrainYVar)
+    for i in range(0, TrainX.shape[0]):
+        TrainY[i] = (TrainY[i] - TrainYMean)/sqrt(TrainYVar)
     
-#     for i in range(0, ValidateX.shape[0]):
-#         ValidateX[i,:] = (ValidateX[i,:] - TrainXMean)/sqrt(TrainXVar)
+    for i in range(0, ValidateX.shape[0]):
+        ValidateX[i,:] = (ValidateX[i,:] - TrainXMean)/sqrt(TrainXVar)
     
-#     for i in range(0, TestX.shape[0]):
-#         TestX[i,:] = (TestX[i,:] - TrainXMean)/sqrt(TrainXVar)
+    for i in range(0, ValidateY.shape[0]):
+        ValidateY[i] = (ValidateY[i] - TrainYMean)/sqrt(TrainYVar)
 
-#     return TrainX, TrainY, ValidateX, ValidateY, TestX, TestY
+    for i in range(0, TestX.shape[0]):
+        TestX[i,:] = (TestX[i,:] - TrainXMean)/sqrt(TrainXVar)
 
-# TrainX, TrainY, ValidateX, ValidateY, TestX, TestY = FromDataFilePLSR_DE_BPSO.getAllOfTheData()
-# TrainX, ValidateX, TestX = FromDataFilePLSR_DE_BPSO.rescaleTheData(TrainX, ValidateX, TestX)
+    for i in range(0, TestY.shape[0]):
+        TestY[i] = (TestY[i] - TrainYMean)/sqrt(TrainYVar)
 
+    return TrainX, TrainY, ValidateX, ValidateY, TestX, TestY
+
+TrainX, TrainY, ValidateX, ValidateY, TestX, TestY = getAllOfTheData()
+TrainX, TrainY, ValidateX, ValidateY, TestX, TestY = rescaleTheData(TrainX, TrainY, ValidateX, ValidateY, TestX, TestY)
 
 ffn = FeedForwardNetwork()
 
 # create layers
-inLayer = LinearLayer(2, name="input")
-hiddenLayer = SigmoidLayer(10, name="hidden1")
-hiddenLayer2 = SigmoidLayer(10, name="hidden2")
+inLayer = LinearLayer(TrainX.shape[1], name="input")
+hiddenLayer = SigmoidLayer(20, name="hidden1")
+# hiddenLayer2 = SigmoidLayer(20, name="hidden2")
 outLayer = LinearLayer(1, name="output")
 
 # add layers to feed forward network
 ffn.addInputModule(inLayer)
 ffn.addModule(hiddenLayer)
-ffn.addModule(hiddenLayer2)
+# ffn.addModule(hiddenLayer2)
 ffn.addOutputModule(outLayer)
 
 ffn.addModule(BiasUnit(name='bias'))
 # establish connections between layers
 in_to_hidden = FullConnection(inLayer, hiddenLayer)
-hidden_to_hidden = FullConnection(hiddenLayer, hiddenLayer2)
-hidden_to_out = FullConnection(hiddenLayer2, outLayer)
+# hidden_to_hidden = FullConnection(hiddenLayer, hiddenLayer2)
+hidden_to_out = FullConnection(hiddenLayer, outLayer)
 
 # add connections to network
 ffn.addConnection(in_to_hidden)
-ffn.addConnection(hidden_to_hidden)
+# ffn.addConnection(hidden_to_hidden)
 ffn.addConnection(hidden_to_out)
 
 # necessary, sort layers into correct/certain order
@@ -81,38 +89,40 @@ ffn.convertToFastNetwork()
 
 ffn.randomize()
 
-print ffn.activate([1, 2])
+# print ffn.activate([1, 2])
 
-print "Input layer weights: {}".format(in_to_hidden.params)
-print "Hidden layer weights: {}".format(hidden_to_out.params)
-print "All weights: {}".format(ffn.params)
 
-ds = SupervisedDataSet(2,1)
+print TrainX
+# print "Input layer weights: {}".format(in_to_hidden.params)
+# print "Hidden layer weights: {}".format(hidden_to_out.params)
+# print "All weights: {}".format(ffn.params)
 
-data = np.random.randn(1000, 2)
-y = np.random.randn(1000, 1)
+ds = SupervisedDataSet(TrainX.shape[1],1)
+
+# data = np.random.randn(1000, 2)
+# y = np.random.randn(1000, 1)
 
 # add data to dataset object (ds)
-for i in range(data.shape[0]):
+for i in range(TrainX.shape[0]):
 	# print data[i]
 	# print y[i]
-	ds.addSample(data[i], y[i])
+	ds.addSample(TrainX[i], TrainY[i])
 
-trainer = BackpropTrainer(ffn, ds, learningrate=.4, momentum=.2)#, verbose=True)
-
-for i in range(10):
+# trainer = BackpropTrainer(ffn, ds, learningrate=.4, momentum=.2)#, verbose=True)
+trainer = BackpropTrainer(ffn, ds)#, learningrate=.4, momentum=.2)#, verbose=True)
+for i in range(1000):
 	error = trainer.train()
 	if i%10 == 0:
 		print "{} error: {}".format(i, error)
 	# print "All weights: {}".format(ffn.params)
-	if error < .45:
+	if error < .0000001:
 		break 
 
 # error = trainer.trainUntilConvergence(maxEpochs=1000)
 
 print "\n"
-for i in range(data.shape[0]):
-	print ffn.activate(data[i]), y[i]
+for i in range(TrainX.shape[0]):
+	print ffn.activate(TrainX[i]), TrainY[i]
 # print ffn.activate(data[0])
 
 
