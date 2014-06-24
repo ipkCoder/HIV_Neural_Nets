3556913fc480552ede977600d5af9a2252d63c98
Ian Kane <iankane@example.com>
2014-06-23 18:06:12 -0700

    training with different learning rates

--- FFN.py
+++ FFN.py
@@ -26,7 +26,7 @@ def getAllOfTheData():
     TestY = placeDataIntoArray('Test-pIC50.csv')
     return TrainX, TrainY, ValidateX, ValidateY, TestX, TestY
 
-def rescaleTheData(TrainX, TrainY, ValidateX, ValidateY, TestX, TestY):
+def rescaleTheData(TrainX, TrainY, TestX, TestY):
 
     # 1 degree of freedom means (ddof) N-1 unbiased estimation
     TrainXVar = TrainX.var(axis = 0, ddof=1)
@@ -39,12 +39,6 @@ def rescaleTheData(TrainX, TrainY, ValidateX, ValidateY, TestX, TestY):
     
     for i in range(0, TrainX.shape[0]):
         TrainY[i] = (TrainY[i] - TrainYMean)/sqrt(TrainYVar)
-    
-    for i in range(0, ValidateX.shape[0]):
-        ValidateX[i,:] = (ValidateX[i,:] - TrainXMean)/sqrt(TrainXVar)
-    
-    for i in range(0, ValidateY.shape[0]):
-        ValidateY[i] = (ValidateY[i] - TrainYMean)/sqrt(TrainYVar)
 
     for i in range(0, TestX.shape[0]):
         TestX[i,:] = (TestX[i,:] - TrainXMean)/sqrt(TrainXVar)
@@ -52,15 +46,19 @@ def rescaleTheData(TrainX, TrainY, ValidateX, ValidateY, TestX, TestY):
     for i in range(0, TestY.shape[0]):
         TestY[i] = (TestY[i] - TrainYMean)/sqrt(TrainYVar)
 
-    return TrainX, TrainY, ValidateX, ValidateY, TestX, TestY
+    return TrainX, TrainY, TestX, TestY
 
 TrainX, TrainY, ValidateX, ValidateY, TestX, TestY = getAllOfTheData()
-TrainX, TrainY, ValidateX, ValidateY, TestX, TestY = rescaleTheData(TrainX, TrainY, ValidateX, ValidateY, TestX, TestY)
+
+TrainValX = append(TrainX, ValidateX, axis=0)
+TrainValY = append(TrainY, ValidateY, axis=0)
+
+TrainValX, TrainValY, TestX, TestY = rescaleTheData(TrainValX, TrainValY, TestX, TestY)
 
 ffn = FeedForwardNetwork()
 
 # create layers
-inLayer = LinearLayer(TrainX.shape[1], name="input")
+inLayer = LinearLayer(TrainValX.shape[1], name="input")
 hiddenLayer = SigmoidLayer(20, name="hidden1")
 # hiddenLayer2 = SigmoidLayer(20, name="hidden2")
 outLayer = LinearLayer(1, name="output")
@@ -97,32 +95,42 @@ print TrainX
 # print "Hidden layer weights: {}".format(hidden_to_out.params)
 # print "All weights: {}".format(ffn.params)
 
-ds = SupervisedDataSet(TrainX.shape[1],1)
+ds = SupervisedDataSet(TrainValX.shape[1],1)
 
 # data = np.random.randn(1000, 2)
 # y = np.random.randn(1000, 1)
 
 # add data to dataset object (ds)
-for i in range(TrainX.shape[0]):
+for i in range(TrainValX.shape[0]):
 	# print data[i]
 	# print y[i]
-	ds.addSample(TrainX[i], TrainY[i])
+	ds.addSample(TrainValX[i], TrainValY[i])
 
 # trainer = BackpropTrainer(ffn, ds, learningrate=.4, momentum=.2)#, verbose=True)
 trainer = BackpropTrainer(ffn, ds)#, learningrate=.4, momentum=.2)#, verbose=True)
-for i in range(1000):
-	error = trainer.train()
-	if i%10 == 0:
-		print "{} error: {}".format(i, error)
-	# print "All weights: {}".format(ffn.params)
-	if error < .0000001:
-		break 
 
-# error = trainer.trainUntilConvergence(maxEpochs=1000)
+alpha = array([.05, .1, .15, .2, .25, .3, 3.5])
 
-print "\n"
-for i in range(TrainX.shape[0]):
-	print ffn.activate(TrainX[i]), TrainY[i]
+for i in range(alpha.shape[0]):
+	trainer = BackpropTrainer(ffn, ds, learningrate=alpha[i]) 
+
+	# for j in range(1000):
+	# 	error = trainer.train()
+	# 	if j%10 == 0:
+	# 		print "{} error: {}".format(j, error)
+	# 	# print "All weights: {}".format(ffn.params)
+	# 	if error < .001:
+	# 		break 
+
+	error = trainer.trainUntilConvergence(maxEpochs=2000, continueEpochs=10)
+
+	print "alpha: {}\n".format(alpha[i])
+	for j in range(TrainValX.shape[0]):
+		print ffn.activate(TrainValX[j]), TrainValY[j]
+
+	print "\n"
+	for j in range(TestX.shape[0]):
+		print ffn.activate(TestX[j]), TestY[j]
 # print ffn.activate(data[0])
 
 
