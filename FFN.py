596b078159e680e00950a3998983df5609a6f2b1
Ian Kane <iankane@example.com>
2014-06-23 20:37:17 -0700

    include momentum in model

--- FFN.py
+++ FFN.py
@@ -51,11 +51,15 @@ def rescaleTheData(TrainX, TrainY, TestX, TestY):
 
 TrainX, TrainY, ValidateX, ValidateY, TestX, TestY = getAllOfTheData()
 
+# combine test and training sets
+# Backprop trainer will split it up
 TrainValX = append(TrainX, ValidateX, axis=0)
 TrainValY = append(TrainY, ValidateY, axis=0)
 
+# rescale data
 TrainValX, TrainValY, TestX, TestY = rescaleTheData(TrainValX, TrainValY, TestX, TestY)
 
+# network object
 ffn = FeedForwardNetwork()
 
 # create layers
@@ -70,7 +74,9 @@ ffn.addModule(hiddenLayer)
 # ffn.addModule(hiddenLayer2)
 ffn.addOutputModule(outLayer)
 
+# add bias unit to layers
 ffn.addModule(BiasUnit(name='bias'))
+
 # establish connections between layers
 in_to_hidden = FullConnection(inLayer, hiddenLayer)
 # hidden_to_hidden = FullConnection(hiddenLayer, hiddenLayer2)
@@ -84,20 +90,16 @@ ffn.addConnection(hidden_to_out)
 # necessary, sort layers into correct/certain order
 ffn.sortModules()
 
+
 ffn.convertToFastNetwork()
 
-ffn.randomize()
-
-# print ffn.activate([1, 2])
 
 # print "Input layer weights: {}".format(in_to_hidden.params)
 # print "Hidden layer weights: {}".format(hidden_to_out.params)
 # print "All weights: {}".format(ffn.params)
 
+# dataset object
 ds = SupervisedDataSet(TrainValX.shape[1],1)
-
-# data = np.random.randn(1000, 2)
-# y = np.random.randn(1000, 1)
 
 # add data to dataset object (ds)
 for i in range(TrainValX.shape[0]):
@@ -105,38 +107,50 @@ for i in range(TrainValX.shape[0]):
 	# print y[i]
 	ds.addSample(TrainValX[i], TrainValY[i])
 
-# trainer = BackpropTrainer(ffn, ds, learningrate=.4, momentum=.2)#, verbose=True)
+# Backprop trainer object
 trainer = BackpropTrainer(ffn, ds)#, learningrate=.4, momentum=.2)#, verbose=True)
 
-alpha = array([0, .05, .1, .2])#, .15, .2, .25, .3, 3.5])
-
-
+# learning rates to test
+# note: best rates have been between .07 and .1, but seems to very inbetween
+# most consistent between .07 and .085
+alpha = array([0, .05, .07, .0775, .085, .1, .2])#, .15, .2, .25, .3, 3.5])
+momentum = array([.05, .1, .15])
+# test learning rates
 for i in range(alpha.shape[0]):
-	trainer = BackpropTrainer(ffn, ds, learningrate=alpha[i]) 
-
-	# for j in range(1000):
-	# 	error = trainer.train()
-	# 	if j%10 == 0:
-	# 		print "{} error: {}".format(j, error)
-	# 	# print "All weights: {}".format(ffn.params)
-	# 	if error < .001:
-	# 		break 
-
-	error = trainer.trainUntilConvergence(maxEpochs=2000, continueEpochs=10)
-
-	print "alpha: {}".format(alpha[i])
 	
-	train_outputs = zeros(TrainValX.shape[0])
-	for j in range(TrainValX.shape[0]):
-		train_outputs[j] = ffn.activate(TrainValX[j])
-		# print train_outputs[j], TrainValY[j]
-	print "Train MSE: {}".format(Validator.MSE(train_outputs, TrainValY))
+	# randomiz weights
+	ffn.randomize()
 
-	test_outputs = zeros(TestX.shape[0])
-	for j in range(TestX.shape[0]):
-		test_outputs[j] = ffn.activate(TestX[j])
-		# print test_outputs[j], TestY[j]
-	print "Test MSE: {}".format(Validator.MSE(test_outputs, TestY))
-# print ffn.activate(data[0])
+	for k in range(momentum.shape[0]):
+		# Backprop trainer object
+		trainer = BackpropTrainer(ffn, ds, learningrate=alpha[i], momentum=momentum[k])#, verbose=True)
+
+		# for j in range(1000):
+		# 	error = trainer.train()
+		# 	if j%10 == 0:
+		# 		print "{} error: {}".format(j, error)
+		# 	# print "All weights: {}".format(ffn.params)
+		# 	if error < .001:
+		# 		break 
+
+		# splits data into 75% training, 25% validation
+		# train until convergence
+		error = trainer.trainUntilConvergence(maxEpochs=2000, continueEpochs=10)# validationPortion=.X
+
+		# print results
+		print "alpha: {}, momentum: {}".format(alpha[i], momentum[k])
+
+		train_outputs = zeros(TrainValX.shape[0])
+		for j in range(TrainValX.shape[0]):
+			train_outputs[j] = ffn.activate(TrainValX[j])
+			# print train_outputs[j], TrainValY[j]
+		print "Train MSE: {}".format(Validator.MSE(train_outputs, TrainValY))
+
+		test_outputs = zeros(TestX.shape[0])
+		for j in range(TestX.shape[0]):
+			test_outputs[j] = ffn.activate(TestX[j])
+			# print test_outputs[j], TestY[j]
+		print "Test MSE: {}".format(Validator.MSE(test_outputs, TestY))
+
 
 
