4db7619ce212f625769c4074a00123fb0b33dd8c
kingrichard2005@gmail.com <kingrichard2005@gmail.com>
2014-07-04 20:44:19 -0700

    Corrections
    -corrected scope of inner "for" loops in main genetic algorithm
    generation loop
    -updated selection function to use tournament based selection with
    selection size of 3.

--- ANN/EA.py
+++ ANN/EA.py
@@ -33,57 +33,56 @@ def evalGAOneMax(individual):
     '''Example evaluation function from DEAP'''
     return sum(individual),
 
-def mainGAEvolve(eaPopulation):
+def coreGAEvolve(EAContainer):
     '''Evolves the feature descriptor mask with genetic prpogramming'''
     try:
         print("Start of GA evolution");
         # Evaluate the entire population
-        for ind, fit in zip(eaPopulation.gaPop, eaPopulation.fitnesses):
+        for ind, fit in zip(EAContainer.gaPop, EAContainer.fitnesses):
             ind.fitness.values = fit;
-        print("  Evaluated %i individuals" % len(eaPopulation.gaPop));
+        print("  Evaluated %i individuals" % len(EAContainer.gaPop));
 
         # Begin the evolution
-        for g in xrange(eaPopulation.num_gen):
+        for g in xrange(EAContainer.num_gen):
             print("-- Generation %i --" % g);
             # Select the next generation individuals
-            offspring = eaPopulation.toolbox.select(eaPopulation.gaPop, len(eaPopulation.gaPop));
+            offspring = EAContainer.toolbox.select(EAContainer.gaPop, len(EAContainer.gaPop));
             # Clone the selected individuals
-            offspring = list(map(eaPopulation.toolbox.clone, offspring));
+            offspring = list(map(EAContainer.toolbox.clone, offspring));
 
-        # Apply crossover and mutation on the offspring
-        for child1, child2 in zip(offspring[::2], offspring[1::2]):
-            if random.Random().random < eaPopulation.cross_prob:
-                eaPopulation.toolbox.mate(child1, child2);
-                del child1.fitness.values;
-                del child2.fitness.values;
+            # Apply crossover and mutation on the offspring
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+                if random.Random().random < EAContainer.cross_prob:
+                    EAContainer.toolbox.mate(child1, child2);
+                    del child1.fitness.values;
+                    del child2.fitness.values;
 
-        for mutant in offspring:
-            if random.Random().random < eaPopulation.mut_prob:
-                eaPopulation.toolbox.mutate(mutant);
-                del mutant.fitness.values;
+            for mutant in offspring:
+                if random.Random().random < EAContainer.mut_prob:
+                    EAContainer.toolbox.mutate(mutant);
+                    del mutant.fitness.values;
 
-        # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses   = map(eaPopulation.toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit;
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses   = map(EAContainer.toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit;
 
-        print("  Evaluated %i individuals" % len(invalid_ind));
-        
-        # The population is entirely replaced by the offspring
-        eaPopulation.gaPop[:] = offspring;
-        # Gather all the fitnesses in one list and print the stats
-        fits   = [ind.fitness.values[0] for ind in eaPopulation.gaPop]
-        length = len(eaPopulation.gaPop)
-        mean   = sum(fits) / length
-        sum2   = sum(x*x for x in fits)
-        std    = abs(sum2 / length - mean**2)**0.5
-        print("  Min %s" % min(fits))
-        print("  Max %s" % max(fits))
-        print("  Avg %s" % mean)
-        print("  Std %s" % std)
+            print("  Evaluated %i individuals" % len(invalid_ind));
+            # The population is entirely replaced by the offspring
+            EAContainer.gaPop[:] = offspring;
+            # Gather all the fitnesses in one list and print the stats
+            fits   = [ind.fitness.values[0] for ind in EAContainer.gaPop]
+            length = len(EAContainer.gaPop)
+            mean   = sum(fits) / length
+            sum2   = sum(x*x for x in fits)
+            std    = abs(sum2 / length - mean**2)**0.5
+            print("  Min %s" % min(fits))
+            print("  Max %s" % max(fits))
+            print("  Avg %s" % mean)
+            print("  Std %s" % std)
         print("-- End of (successful) evolution --")
-        best_ind = tools.selBest(eaPopulation.gaPop, 1)[0]
+        best_ind = tools.selBest(EAContainer.gaPop, 1)[0]
         print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
         return 0;
     except:
@@ -93,30 +92,30 @@ class EA:
     '''Class container for an evolutionary algorithm'''
     def __init__(this
                  ,featureDescriptors = 396
-                 ,populationsize = 100
-                 ,cross_prob = .5
-                 ,mut_prob  = .5
-                 ,num_gen = 5
-                 ,crosschance  = .5
-                 ,mutchance  = .5
-                 ,gen_max = 100):
+                 ,populationsize     = 100
+                 ,cross_prob         = .5
+                 ,mut_prob           = .5
+                 ,num_gen            = 5
+                 ,crosschance        = .5
+                 ,mutchance          = .5
+                 ,gen_max            = 100):
         this.featureDescriptors = featureDescriptors
         this.populationsize     = populationsize
         #Fitness
         creator.create("FitnessMax", base.Fitness, weights=(1.0,))
         this.toolbox = base.Toolbox();
-        #Individual Attribute
+        # Register attribute
         this.toolbox.register("attr_bool", createIndividualAttribute)
 
-        # Define an individual as a list representing the binary
+        # Register agent definition as a list representing the binary
         # feature mask
         creator.create("Individual", list, fitness=creator.FitnessMax)
 
-        # Individual Creation
+        # Register agent creation
         this.toolbox.register("individual", tools.initRepeat, creator.Individual,
                     this.toolbox.attr_bool, featureDescriptors)
         
-        #Population Creation
+        # Register population creation
         this.toolbox.register("population", tools.initRepeat, list, this.toolbox.individual)
 
         # Initialize population
@@ -135,10 +134,10 @@ class EA:
         """
         this.toolbox.register("evaluate",evalGAOneMax)
         # configure a single-point cross-over
-        this.toolbox.register("mate", tools.cxOnePoint)
+        this.toolbox.register("mate", tools.cxESTwoPoint)
         # configure a .05 probability of mutation
         this.toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-        this.toolbox.register("select", tools.selBest)
+        this.toolbox.register("select", tools.selTournament, tournsize = 3)
 
         # Collect population's initial fitness'
         this.fitnesses  = list(map(this.toolbox.evaluate,this.gaPop))
