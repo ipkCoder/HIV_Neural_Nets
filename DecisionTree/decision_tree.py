944df5e47182af6887e0fb26b471dfb9ab94123a
Ian Kane <iankane@Ian-Kanes-MacBook-Pro.local>
2014-08-19 10:03:51 -0700

    Split data. Train and validate

--- DecisionTree/decision_tree.py
+++ DecisionTree/decision_tree.py
@@ -1,12 +1,24 @@
+# Reference: http://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeRegressor.html
 
-from ImportData import *
+from DataFile import *
+#from ImportData import *
 from sklearn import tree
 from sklearn.externals.six import StringIO
 import pydot
 
-data, target = getAllOfTheData()
+#data, target = getAllOfTheData()
+trainX, trainY, valX, valY, testX, testY = getAllOfTheData()
+
 rt = tree.DecisionTreeRegressor(min_samples_split=5, min_samples_leaf=3)
-rt = rt.fit(data, target)
+
+rt = rt.fit(trainX, trainY)
+#rt = rt.fit(data, target)
+
+pred = rt.predict(valX)
+for i in range(len(pred)):
+    print("{}\t{}".format(valY[i], pred[i]))
+r2pred = rt.score(valX, valY)
+print("R^2 val = {}".format(r2pred))
 
 dot_data = StringIO()
 tree.export_graphviz(rt, out_file=dot_data)
